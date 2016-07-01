81050f13bdf276156e1ef746c2acf5cc4e14d4af
ChandanWadhwa <chandan.wadhwa@oodlestechnologies.com>
2016-07-01 16:55:02 +0530

    Fragment Backstack Added

---
+++ Fragmentbackstack/app/src/main/java/opencv/demo/com/fragmentbackstack/FragmentA.java
@@ -0,0 +1,42 @@
+package opencv.demo.com.fragmentbackstack;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentA extends Fragment {
+
+    public FragmentA() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_, container, false);
+    }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+
+}
---
+++ Fragmentbackstack/app/src/main/java/opencv/demo/com/fragmentbackstack/MainActivity.java
@@ -0,0 +1,67 @@
+package opencv.demo.com.fragmentbackstack;
+
+
+
+
+import android.databinding.DataBindingUtil;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.logging.Logger;
+
+import opencv.demo.com.fragmentbackstack.databinding.MainActivityBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        MainActivityBinding binding=  DataBindingUtil.setContentView(this, R.layout.main_activity);
+        binding.addA.setOnClickListener(this);
+        binding.addB.setOnClickListener(this);
+        binding.addC.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.addA:
+                addFragment("A",new FragmentA());
+                break;
+            case R.id.addB:
+                addFragment("B",new FragmentB());
+                break;
+            case R.id.addC:
+                addFragment("C",new FragmentC());
+                break;
+        }
+       // addFragment("sdsd");
+    }
+
+
+    private void addFragment(String fragmentName,Fragment frg)
+    {
+        Fragment frag_A=frg;
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.layout_fragment, frag_A,fragmentName);
+        //ft.replace(resId, fragmentB);
+        ft.addToBackStack(fragmentName);
+        ft.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Log.d("MainActivity", "onBackPressed: "+getSupportFragmentManager().getBackStackEntryCount());
+        for(int i=getSupportFragmentManager().getBackStackEntryCount()-1;i>=0;i--) {
+            Log.d("MainActivity", "onBackPressed: "+getSupportFragmentManager().getBackStackEntryAt(i).getName());
+        }
+
+    }
+}
